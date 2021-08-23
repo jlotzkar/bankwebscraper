cfb624817b8a306209401d27d09a32d5e7aa44ad
jlotzkar <38224475+jlotzkar@users.noreply.github.com>
2021-08-22 21:00:48 -0700

    Scotia scraping

--- innovationfinal.py
+++ innovationfinal.py
@@ -276,3 +276,110 @@ dfTD = pd.concat(dfs)
 #deleting duplicate rows
 
 dfTD = dfTD.drop_duplicates()
+
+
+#%% Scraping Scotia
+
+
+
+#reading URL, saving it
+
+req = Request(urlScotia, headers={'User-Agent': 'Mozilla/5.0'})
+
+webpage = urlopen(req).read()
+
+soup = BeautifulSoup(webpage, "lxml")
+
+ 
+
+dates = []
+
+articles = []
+
+links = []
+
+sections = soup.findAll("div", {"class": "wd_item_wrapper"})
+
+ 
+
+#searching through website and collecting dates, titles, etc.
+
+for section in sections:
+
+    date = section.find("div",{"class":"wd_date"})
+
+    title = section.find("div",{"class":"wd_title"})
+
+    dates.append(date.text.strip())
+
+    articles.append(title.text.strip())
+
+    link = title.a['href']
+
+    links.append(link)
+
+ 
+
+#saving to dataframe
+
+dfScotia = pd.DataFrame({"Date":dates, "Title":articles, "Link":links})
+
+ 
+
+#making date as index for dataframe
+
+#dfScotia.set_index(['Date'], drop = True, inplace = True)
+
+ 
+
+#convert to datetime
+
+dfScotia['Date'] = dfScotia['Date'].apply(pd.to_datetime)
+
+ 
+
+#filtering through titles with the following words (innovation-related trends)
+
+dfs = []
+
+keywords = ['new', 'technology', 'innovation', 'innovative', 'innovate', 'innovates', 'innovating', 'digital', 'crypto', 'blockchain', 'unveil', 'unveils', 'unveiling', 'launch', 'launches', 'launching', 'announce', 'announces', 'announcing', 'announcement', 'introduce', 'introduces', 'introducing', 'introduction']
+
+for key in keywords:
+
+    filterScotia =  dfScotia[dfScotia['Title'].str.contains(key)]
+
+    dfs.append(filterScotia)
+
+   
+
+#checking capitalized words
+
+for key in keywords:
+
+    filterScotia =  dfScotia[dfScotia['Title'].str.contains(key.capitalize())]
+
+    dfs.append(filterScotia)
+
+ 
+
+#final Scotia dataframe
+
+dfScotia = pd.concat(dfs)
+
+   
+
+#convert to datetime
+
+#dfScotia.index = pd.to_datetime(dfScotia.index)
+
+ 
+
+#sorting by date
+
+#dfScotia = dfScotia.sort_index(ascending=False)
+
+ 
+
+#deleting duplicate rows
+
+dfScotia = dfScotia.drop_duplicates()
